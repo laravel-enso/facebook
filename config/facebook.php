de1c628e698caac5aee1569af699589084948f09
ALIN <=>
2025-07-11 16:10:19 +0300

    updated AppServiceProvider

---
+++ config/facebook.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'verificationCode' => env('FACEBOOK_VERIFICATION_CODE'),
+    'pageId' => env('FACEBOOK_PAGE_ID'),
+];
--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -8,12 +8,31 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->load()->publish();
     }
 
     public function register()
     {
         //
+    }
+
+    private function load(): self
+    {
+        $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
+
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        $this->mergeConfigFrom(__DIR__.'/../config/facebook.php', 'enso.facebook');
+
+        return $this;
+    }
+
+    private function publish(): self
+    {
+        $this->publishes([
+            __DIR__.'/../config' => config_path('enso'),
+        ], ['facebook-config', 'enso-config']);
+
+        return $this;
     }
 }
