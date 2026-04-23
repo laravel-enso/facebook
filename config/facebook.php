43dbb5c630730e963a1b26542078b4e6ef085100
Manuela Mindroc <manuelamindroc@MacBook-Pro-Manuela-2.local>
2026-04-23 12:21:56 +0300

    fixes upgrade

--- config/facebook.php
+++ config/facebook.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
+    'verificationCode' => env('FACEBOOK_VERIFICATION_CODE'),
+    'pageId' => env('FACEBOOK_PAGE_ID'),
     'settingsId' => env('FACEBOOK_SETTINGS_ID', 1),
 ];
--- src/Upgrades/AddFacebookTableColumns.php
+++ src/Upgrades/AddFacebookTableColumns.php
@@ -3,16 +3,19 @@
 namespace LaravelEnso\Facebook\Upgrades;
 
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Schema;
+use LaravelEnso\Facebook\Models\Settings;
+use LaravelEnso\Upgrade\Contracts\MigratesData;
 use LaravelEnso\Upgrade\Contracts\MigratesTable;
 use LaravelEnso\Upgrade\Contracts\ShouldRunManually;
 use LaravelEnso\Upgrade\Helpers\Table;
 
-class AddFacebookTableColumns implements MigratesTable, ShouldRunManually
+class AddFacebookTableColumns implements MigratesTable, MigratesData, ShouldRunManually
 {
     public function isMigrated(): bool
     {
-        return Table::hasColumn('facebook_settings', 'recaptcha_secret');
+        return Table::hasColumn('facebook_settings', 'verification_code');
     }
 
     public function migrateTable(): void
@@ -21,5 +24,13 @@ class AddFacebookTableColumns implements MigratesTable, ShouldRunManually
             $table->string('verification_code')->nullable();
             $table->string('page_id')->nullable();
         });
+    }
+
+    public function migrateData(): void
+    {
+        Settings::current()->update([
+            'verification_code' => Config::get('enso.google.verificationCode'),
+            'page_id' => Config::get('enso.google.pageId'),
+        ]);
     }
 }
